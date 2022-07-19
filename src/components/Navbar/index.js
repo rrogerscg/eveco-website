7d85f9b048b4a79beb4b0b39d5cfaff02cdbbadc
Russell Rogers <rrogerscg@gmail.com>
2022-07-19 11:39:11 -0700

    fixed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,9 +3,9 @@ import {Link, useMatch, useResolvedPath} from "react-router-dom";
 
 export default function Navbar() {
     return <nav className="nav">
-        <Link to="/" className="site-title">EvEco</Link>
+        <Link to="/eveco-website" className="site-title">EvEco</Link>
         <ul>
-            <CustomLink to="/">EvEco</CustomLink>
+            <CustomLink to="/eveco-website">EvEco</CustomLink>
             <CustomLink to="/projects">Projects</CustomLink>
             <CustomLink to="/pricing">Pricing</CustomLink>
             <CustomLink to="/about">About</CustomLink>
