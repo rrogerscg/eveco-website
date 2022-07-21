aa28c008407cfa3de3c98a9d069a3f5b6057a107
Russell Rogers <rrogerscg@gmail.com>
2022-07-20 17:04:59 -0700

    visual updates

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,11 @@ export default function Home() {
     return (
         <div>
             <h1 className="hero-title">EvEco</h1>
-            <hr />
+            <p>This is a small blurb about the company and its goals.</p>
+            <br />
+            <hr className="container-item"/>
+            <br />
+            <i className="bi-alarm"></i>
         </div>
     );
 }
