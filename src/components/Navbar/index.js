1c9e30142406725ae63fdd34032d46b1cf6d5bd7
Russell Rogers <rrogerscg@gmail.com>
2022-07-24 08:03:56 -0700

    adjusted amount of cards per row and added black line across navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,22 @@
 import {Link, useMatch, useResolvedPath} from "react-router-dom";
 
 export default function Navbar() {
-    return <nav className="nav bg-color-primary">
-        <Link to="/eveco-website" className="site-title">EvEco</Link>
-        <ul>
-            <CustomLink to="/eveco-website">Home</CustomLink>
-            <CustomLink to="/eveco-website/services">Services</CustomLink>
-            <CustomLink to="/eveco-website/projects">Projects</CustomLink>
-            <CustomLink to="/eveco-website/pricing">Pricing</CustomLink>
-            <CustomLink to="/eveco-website/about">About</CustomLink>
-            <CustomLink to="/eveco-website/contact">Contact</CustomLink>
-        </ul>
-    </nav>
+    return (
+        <nav className="nav bg-color-primary">
+            <Link to="/eveco-website" className="site-title">EvEco</Link>
+            <div className="link-container">
+                <ul>
+                    <CustomLink to="/eveco-website">Home</CustomLink>
+                    <CustomLink to="/eveco-website/services">Services</CustomLink>
+                    <CustomLink to="/eveco-website/projects">Projects</CustomLink>
+                    <CustomLink to="/eveco-website/pricing">Pricing</CustomLink>
+                    <CustomLink to="/eveco-website/about">About</CustomLink>
+                    <CustomLink to="/eveco-website/contact">Contact</CustomLink>
+                </ul>
+            </div>
+
+        </nav>
+    )
 }
 
 function CustomLink({to, children, ...props}) {
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -4,17 +4,19 @@ export default function Projects() {
     return (
         <div className="projects-page">
             <h1 className="page-header">Projects</h1>
-            <div className="cards-container page-item">
-                <Card className="page-item" title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
-                <Card title="test title" footer="test footer"/>
+            <div  className="content-container ">
+                <div className="cards-container page-item">
+                    <Card className="page-item" title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                    <Card title="test title" footer="test footer"/>
+                </div>
             </div>
         </div>
     )
